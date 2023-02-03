0ac8480079db532455d231b3f5edab407deaf890
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-02-03 00:21:54 -0500

    feat: add api gateway

--- lib/aws-cdk-playground-stack.ts
+++ lib/aws-cdk-playground-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class AwsCdkPlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'HelloHandler', {
+    const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
     });
   }
 }
