02b03b10cb67c1ed849b07c2cc53b9f15f8661ed
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-02-02 23:18:51 -0500

    refactor: remove variable

--- bin/aws-cdk-playground.ts
+++ bin/aws-cdk-playground.ts
@@ -3,8 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkPlaygroundStack } from '../lib/aws-cdk-playground-stack';
 
-const app = new cdk.App();
-new AwsCdkPlaygroundStack(app, 'AwsCdkPlaygroundStack', {
+new AwsCdkPlaygroundStack(new cdk.App(), 'AwsCdkPlaygroundStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
