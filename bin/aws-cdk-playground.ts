6dbb16deb5f37925a6f37c983862352d490e7973
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-04-01 00:07:19 -0400

    refactor: reorganize code

--- bin/aws-cdk-playground.ts
+++ bin/aws-cdk-playground.ts
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
-import { ApiGatewayStack } from '../lib/api-gateway-stack.js';
-import { CodeBuildStack } from '../lib/code-build-stack.js';
-import { CodePipelineStack, LambdaStack } from '../lib/code-pipeline-stack.js';
+import { ApiGatewayStack } from '../lib/api-gateway/api-gateway-stack.js';
+import { CodeBuildStack } from '../lib/code-build/code-build-stack.js';
+import {
+  CodePipelineStack,
+  LambdaStack,
+} from '../lib/code-pipeline/code-pipeline-stack.js';
 
 const app = new cdk.App();
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
-new ApiGatewayStack(app, 'ApiGatewayStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new ApiGatewayStack(app, 'ApiGatewayStack', { env });
 
-new CodeBuildStack(app, 'CodeBuildStack');
+new CodeBuildStack(app, 'CodeBuildStack', { env });
 
-const { lambdaCode } = new LambdaStack(app, 'LambdaStack');
+const { lambdaCode } = new LambdaStack(app, 'LambdaStack', { env });
 
-new CodePipelineStack(app, 'CodePipelineStack', lambdaCode);
+new CodePipelineStack(app, 'CodePipelineStack', lambdaCode, { env });
